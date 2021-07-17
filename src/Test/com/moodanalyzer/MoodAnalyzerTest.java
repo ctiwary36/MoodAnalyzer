31ce53deb002b5343b6f2c231aa7c88b8a814414
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-18 03:36:51 +0530

    refactor

--- src/Test/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/Test/com/moodanalyzer/MoodAnalyzerTest.java
@@ -4,53 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
     public class MoodAnalyzerTest {
-        // Test-Cases for Sad Mood
+        // Test-Case for SadMood
         @Test
-        public void givenMessage_whenSad_ShouldReturnSad() throws MoodAnalysisException {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Sad Mood");
+        public void givenMessage_whenSad_ShouldReturnSad() {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Sad");
             String actual = moodAnalyzer.analyseMood();
             String expected = "SAD";
             Assert.assertEquals(expected, actual);
+
         }
 
+        // Test-Case for HappyMood
         @Test
-        public void givenMessage_whenSAD_ShouldReturnSAD() throws MoodAnalysisException {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in SAD Mood");
-            String actual = moodAnalyzer.analyseMood();
-            String expected = "SAD";
-            Assert.assertEquals(expected, actual);
-        }
-
-        // Test-Cases for Happy Mood
-        @Test
-        public void givenMessage_whenHappy_ShouldReturnHappy() throws MoodAnalysisException {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+        public void givenMessage_whenHappy_ShouldReturnHappy() {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Happy");
             String actual = moodAnalyzer.analyseMood();
             String expected = "HAPPY";
             Assert.assertEquals(expected, actual);
-        }
 
-        // Test-Cases for Null
-        @Test
-        public void givenMessage_whenNull_ShouldThrowMoodAnalysisException() {
-            try {
-                MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-                moodAnalyzer.analyseMood();
-            } catch (MoodAnalysisException e) {
-                Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.exceptionType);
-            }
-        }
-
-        // Test-Cases for Empty
-        @Test
-        public void givenMessage_whenEmpty_ShouldThrowMoodAnalysisException() {
-            try {
-                MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
-                moodAnalyzer.analyseMood();
-            } catch (MoodAnalysisException e) {
-                System.out.println("here");
-                Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, e.exceptionType);
-            }
         }
     }
 
--- src/main/com/moodanalyzer/MoodAnalyzer.java
+++ src/main/com/moodanalyzer/MoodAnalyzer.java
@@ -3,23 +3,25 @@ package main.com.moodanalyzer;
 public class MoodAnalyzer {
     private String message;
 
+    // Default Constructor
     public MoodAnalyzer() {
+        this.message = null;
+
     }
 
+    // Parameterised Constructor
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message == "")
-                throw new MoodAnalysisException("Invalid message", MoodAnalysisException.ExceptionType.ENTERED_EMPTY);
-            if (message.toLowerCase().contains("sad"))
-                return "SAD";
-            return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException("Invalid message", MoodAnalysisException.ExceptionType.ENTERED_NULL);
-        }
+    // Check Sad or Happy Mood
+    public String analyseMood() {
+
+        if (message.toLowerCase().contains("sad"))
+            return "SAD";
+
+        return "HAPPY";
+
     }
 }
 
